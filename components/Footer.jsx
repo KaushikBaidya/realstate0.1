578ce2878e4269d8c18bdff5b7cf233749c0901e
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-01-08 10:50:55 +0600

    added footer, navbar, search filter

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+
+export const Footer = () => {
+  return (
+    <Box
+      textAlign={'center'}
+      p={'5'}
+      color={'gray.600'}
+      borderTop={'1'}
+      borderColor={'gray.100'}
+    >
+      2022 RealEstate, Inc.
+    </Box>
+  )
+}
